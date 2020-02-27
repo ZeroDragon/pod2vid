285cb07e22e17de3105530067fec2beee3f459f7
Zero Dragon <carlos@floresbenavides.com>
2020-02-26 22:09:56 -0600

    Better showwaves position

--- videoGenerator/pod2vid.sh
+++ videoGenerator/pod2vid.sh
@@ -11,7 +11,7 @@ do
   ffmpeg -y -i "$file" -i "$image" -filter_complex \
   "[1:v]scale=w=1280:h=720,pad=1280:720:0:0[image]; \
    [0:a]showwaves=s=1280x200:mode=cline:colors=#740165[sw]; \
-   [image][sw]overlay=0:0[out]" \
+   [image][sw]overlay=0:550[out]" \
   -map "[out]" -map 0:a -c:v libx264 -preset fast -c:a aac -b:a 128k "${filename}.mp4"
 
 done
